b13d07853376255fc57d24fed798b7946766d7c9
Micah Daise <micah.daise@nwktc.edu>
2022-02-23 08:31:15 -0700

    Initial Commit

---
+++ 2022/Paths Demo/Paths Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Paths Demo
+//
+//  Created by Micah Daise on 2/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Awesome Paths Demo")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# codeJournal
+# Micah Daise's codeJournal
+
+## 2022
+### February
+[February 23, 2022](2022/Feb23.md)
